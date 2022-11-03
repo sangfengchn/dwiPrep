9cbd8a86b482b27b1f5fbe8dfaf398be6ae12d28
Feng Sang <sangfengchn@outlook.com>
2022-11-03 10:28:58 +0800

    update: the diffusion number in different run is not same will occur error.

--- code/util/sf_CombineGradient.py
+++ code/util/sf_CombineGradient.py
@@ -6,8 +6,10 @@ import sys
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-subPath = os.path.abspath(sys.argv[1])
-subDerPath = os.path.abspath(sys.argv[2])
+# subPath = os.path.abspath(sys.argv[1])
+# subDerPath = os.path.abspath(sys.argv[2])
+subPath = os.path.abspath('test/sub-44JIANGHONGXIAN')
+subDerPath = os.path.abspath('test/der/')
 # logging.info(subPath)
 
 # combine bval
@@ -15,18 +17,24 @@ bval = []
 bvec = []
 nSample = 0
 for i in sorted(glob(opj(subPath, 'dwi', '*.bval'))):
-    # logging.info(i)
+    logging.info(i)
     bval.append(np.loadtxt(i))
     bvec.append(np.loadtxt(i.replace('.bval', '.bvec')))
     nSample += bval[-1].shape[0]
+    logging.info(bval[-1].shape)
+    logging.info(bvec[-1].shape)
 
 bvalComb = np.zeros(shape = [1, nSample])
 bvecComb = np.zeros(shape = [3, nSample])
+curIndex = 0
 for i in range(len(bvec)):
     tmpBval = bval[i]
-    bvalComb[0, i*tmpBval.shape[0]:(i+1)*tmpBval.shape[0]] = tmpBval
+    # logging.info(tmpBval.shape)
+    bvalComb[0, curIndex:curIndex+tmpBval.shape[0]] = tmpBval
     tmpBvec = bvec[i]
-    bvecComb[:, i*tmpBvec.shape[1]:(i+1)*tmpBvec.shape[1]] = tmpBvec
+    # logging.info(tmpBvec.shape)
+    bvecComb[:, curIndex:curIndex+tmpBvec.shape[1]] = tmpBvec
+    curIndex += tmpBval.shape[0]
 
 # comGradientMatrix = np.concatenate((bvecComb, bvalComb), axis=0).T
 np.savetxt(opj(subDerPath, 'dwi.bval'), bvalComb)
