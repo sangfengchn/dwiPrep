dcc9fc04668e3b0394579ccd3641c6f3cd0baee9
Feng Sang <sangfengchn@outlook.com>
2023-04-17 00:43:46 +0800

    Fis some bugs in g_DeterministicNetwork.m.

--- code/sf_check_preprocess_dwi.py
+++ code/sf_check_preprocess_dwi.py
@@ -7,13 +7,20 @@ import logging
 logging.basicConfig(level = logging.INFO, format = "%(asctime)s %(message)s")
 
 proj = "/brain/zhanjunzhang/Desktop/LIZIlin/HTN_duration_multiple_network"
+raw = opj(proj, "rawdata")
 der = opj(proj, "derivatives", "preprocess_dwi")
 
-for i in glob(opj(der, "sub-*")):
+derSubLists = [os.path.split(i)[-1] for i in glob(opj(der, "sub-*"))]
+for i in glob(opj(raw, "sub-*")):
     subId = os.path.split(i)[-1]
-    # logging.info(subId)
-    if not os.path.exists(opj(i, "Network", "Deterministic", "tracks_filtered_ROIVoxelSize_BN_Atlas_246_1mm_246.txt")):
-        logging.info(subId)
-        shutil.rmtree(i)
+    if subId not in derSubLists:
+        logging.error(subId)
+
+# for i in glob(opj(der, "sub-*")):
+#     subId = os.path.split(i)[-1]
+#     # logging.info(subId)
+#     if not os.path.exists(opj(i, "Network", "Deterministic", "tracks_filtered_ROIVoxelSize_BN_Atlas_246_1mm_246.txt")):
+#         logging.info(subId)
+#         shutil.rmtree(i)
 
 logging.info("Done.")
