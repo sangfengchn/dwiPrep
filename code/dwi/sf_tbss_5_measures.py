cb7801f5fad3a6f414e66ba03baf08c9de9b97b0
Feng Sang <sangfengchn@outlook.com>
2023-07-19 20:32:47 +0800

    Update sf_tbss_5_measures.py

--- code/dwi/sf_tbss_5_measures.py
+++ code/dwi/sf_tbss_5_measures.py
@@ -16,14 +16,15 @@ logging.basicConfig(level=logging.DEBUG)
 
 # project path
 proj = '/brain/zhanjunzhang/Desktop/LIZIlin/HTN_duration'
-der = opj(proj, 'derivatives', 'tbss_fa', 'data', 'stats')
+der = opj(proj, 'derivatives', 'tbss_renew', 'data', 'stats')
+outPath = opj(der, 'Mean_lz_MDt_tfce_corrp_tstat12_pos_sig_mask_20230719.xlsx')
 
 # inputs
 aPath = opj(proj, 'resource', 'atlas', 'JHU-ICBM-tracts-maxprob-thr25-1mm.nii.gz')
 aInfoPath = opj(proj, 'resource', 'atlas', 'JHU-tracts.csv')
-pPath = opj(der, 'lz_tfce_corrp_tstat3_pos.nii.gz')
-skePath = opj(der, 'all_FA_skeletonised.nii.gz')
-outPath = opj(der, 'Mean_FA_lz_tfce_corrp_tstat3_pos.xlsx')
+pPath = opj(der, 'lz_MDt_tfce_corrp_tstat12_pos_sig_mask.nii.gz')
+skePath = opj(der, 'all_MDt_skeletonised.nii.gz')
+
 
 # atlas information
 aInfo = pd.read_csv(aInfoPath, header=0, index_col=0)
@@ -31,6 +32,7 @@ aData = nib.load(aPath).get_fdata()
 
 # binarlized p map
 pData = nib.load(pPath).get_fdata()
+pData[np.isnan(pData)] = 0
 pData[pData < 0.95] = 0
 pData[pData != 0] = 1
 
@@ -41,13 +43,20 @@ resDf = pd.DataFrame()
 for i in range(skeData.shape[3]):
     logging.info(f'Processing sub-{i+1}')
     tmpSkeData = skeData[:, :, :, i]
-    tmpSkeDataSig = tmpSkeData * pData
-    
+    tmpSkeData[np.isnan(tmpSkeData)] = 0
+
+    tmpSkeDataMask = np.zeros(shape = tmpSkeData.shape)
+    tmpSkeDataMask[tmpSkeData != 0] = 1
+    tmpSkeDataSigMask = tmpSkeDataMask * pData
+
     tmpRow = {'SUBORDER': [i+1]}
     for j in aInfo.index.values:
         tmpAtlasData = np.zeros(aData.shape)
         tmpAtlasData[aData == j] = 1
-        tmpRow[aInfo.loc[j, 'Abbr']] = [np.nanmean(tmpAtlasData * tmpSkeDataSig)]
+        tmpAtlasSkeSigMask = tmpAtlasData * tmpSkeDataSigMask
+
+        tmpRow[aInfo.loc[j, 'Abbr']] = [np.nanmean(tmpSkeData[tmpAtlasSkeSigMask == 1])]
+
     resDf = pd.concat([resDf, pd.DataFrame(tmpRow)])
 
 # save result
